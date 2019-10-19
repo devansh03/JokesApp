ed0d5d0aeaba0a390aca7c61dca43cc311ebb26f
Devansh Baldwa <devanshbaldwa@gmail.com>
2019-10-19 16:57:39 +0530

    Repo was made and basics were done.

---
+++ android/app/src/main/java/com/devansh03/jokes/jokes_app/MainActivity.java
@@ -0,0 +1,13 @@
+package com.devansh03.jokes.jokes_app;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
